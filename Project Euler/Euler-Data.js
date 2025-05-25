379ebc2e396abeac2a251635c548e1132d5832db
kellytheace <max.a.kelly@gmail.com>
2025-05-25 09:00:18 +0100

    Update Euler-Data.js

--- Project Euler/Euler-Data.js
+++ Project Euler/Euler-Data.js
@@ -1,25 +1,50 @@
+class EulerProblem {
+  constructor(number, title, description, solutionCode) {
+    this.number = number;
+    this.title = title;
+    this.description = description;
+    this.solutionCode = solutionCode;
+  }
+
+  display() {
+    return `
+      Problem ${this.number}: ${this.title}
+      ${this.description}
+      
+      Code:
+      ${this.solutionCode}
+    `;
+  }
+
+  // You could also add a method to run the solution, if safe
+  log() {
+    console.log(this.display());
+  }
+}
+
+// Now use instances of the class
 const eulerSolutions = [
-  {
-    number: 1,
-    title: "Multiples of 3 and 5",
-    description: "Find the sum of all the multiples of 3 or 5 below 1000.",
-    solution: `let sum = 0;
+  new EulerProblem(
+    1,
+    "Multiples of 3 and 5",
+    "Find the sum of all the multiples of 3 or 5 below 1000.",
+    `let sum = 0;
 for (let i = 0; i < 1000; i++) {
   if (i % 3 === 0 || i % 5 === 0) {
     sum += i;
   }
 }
 console.log(sum);`
-  },
-  {
-    number: 2,
-    title: "Even Fibonacci numbers",
-    description: "Sum the even-valued terms of the Fibonacci sequence below 4 million.",
-    solution: `let a = 1, b = 2, sum = 0;
+  ),
+  new EulerProblem(
+    2,
+    "Even Fibonacci numbers",
+    "Sum the even-valued terms of the Fibonacci sequence below 4 million.",
+    `let a = 1, b = 2, sum = 0;
 while (b < 4000000) {
   if (b % 2 === 0) sum += b;
   [a, b] = [b, a + b];
 }
 console.log(sum);`
-  }
+  )
 ];
