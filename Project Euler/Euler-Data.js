6fe00a7d17c83f4330b5f4517ec96a8d54f10d2d
kellytheace <max.a.kelly@gmail.com>
2025-05-27 14:12:46 +0100

    Update Euler-Data.js

--- Project Euler/Euler-Data.js
+++ Project Euler/Euler-Data.js
@@ -28,13 +28,11 @@ const eulerSolutions = [
     1,
     "Multiples of 3 and 5",
     "Find the sum of all the multiples of 3 or 5 below 1000.",
-    `let sum = 0;
-for (let i = 0; i < 1000; i++) {
-  if (i % 3 === 0 || i % 5 === 0) {
-    sum += i;
-  }
-}
-console.log(sum);`
+    `result=0
+for i in range (1,1000):
+    if i%3 == 0 or i%5 == 0:
+        result=result+i
+print(result)`
   ),
   new EulerProblem(
     2,
