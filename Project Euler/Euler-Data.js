1088a43f5c7750e1905fa3328d1d9bedd0988d95
kellytheace <max.a.kelly@gmail.com>
2025-06-20 21:41:40 +0100

    Update Euler-Data.js

--- Project Euler/Euler-Data.js
+++ Project Euler/Euler-Data.js
@@ -662,5 +662,38 @@ for word in names:
 
 print(resultCount)`,
     "871198282"
+  ),
+  new EulerProblem(
+    23,
+    "Non-Abundant Sums",
+    "A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 29 would be 1+2+4+7+14=28, which means that 28 is a perfect number. A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n. As 12 is the smallest abundant number, 1+2+3+4+16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit. Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.",
+    ["divisor-sum"],
+    `LIMIT = 28123
+
+def sum_factors(n):
+    total = 1
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            total += i
+            if i != n // i:
+                total += n // i
+    return total
+
+abundant = [i for i in range(12, LIMIT + 1) if sum_factors(i) > i]
+
+can_be_written = [False] * (LIMIT + 1)
+
+for i in range(len(abundant)):
+    for j in range(i, len(abundant)):
+        s = abundant[i] + abundant[j]
+        if s <= LIMIT:
+            can_be_written[s] = True
+        else:
+            break
+
+total = sum(i for i in range(1, LIMIT + 1) if not can_be_written[i])
+
+print("Total:", total)`,
+    "4179871"
   )
 ];
