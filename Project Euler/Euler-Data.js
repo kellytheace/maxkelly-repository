4d38c4d042ffafb1bfec06555b832a058b80df2b
kellytheace <max.a.kelly@gmail.com>
2025-08-13 21:28:12 +0100

    Update Euler-Data.js

--- Project Euler/Euler-Data.js
+++ Project Euler/Euler-Data.js
@@ -1647,10 +1647,36 @@ for i in range(3, 10000, 2):  # only odd numbers
   new EulerProblem(
     47,
     "X",
-    "X",
-    ["X"],
-    `X`,
-    "X"
+    "The first two consecutive numbers to have two distinct prime factors are: 14 = 2 × 7, 15 = 3 × 5. The first three consecutive numbers to have three distinct prime factors are: 644 = 2² × 7 × 23, 645 = 3 × 5 × 43, 646 = 2 × 17 × 19. Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?",
+    ["prime-number","divisibility"],
+    `import math
+
+def uniquePrimeFactorsLength(n):
+    factors = set()  # use a set for uniqueness
+
+    # Handle factor 2 separately
+    while n % 2 == 0:
+        factors.add(2)
+        n //= 2
+    # Handle odd factors
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            factors.add(i)
+            n //= i
+    # If n is a prime > 2
+    if n > 2:
+        factors.add(n)
+
+    return len(sorted(factors))
+
+for i in range(1,10000000):
+    if uniquePrimeFactorsLength(i) == 4:
+        if uniquePrimeFactorsLength(i+1) == 4:
+            if uniquePrimeFactorsLength(i+2) == 4:
+                if uniquePrimeFactorsLength(i+3) == 4:
+                    print("result is:",i)
+                    break`,
+    "134043"
   ),
   new EulerProblem(
     48,
